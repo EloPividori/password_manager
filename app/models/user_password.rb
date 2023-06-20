d8fc79a99a73fbf4b7cce3d328d9129e3079ac19
Eloise Pividori <eloisepividori@gmail.com>
2023-06-20 10:21:51 +0200

    added a migrtion to the UserPassword table for roles

--- app/models/user_password.rb
+++ app/models/user_password.rb
@@ -1,4 +1,11 @@
 class UserPassword < ApplicationRecord
+  # View can only view the password
+  # Editor can update the password
+  # Owner can update the password and share the password
+
+  ROLES = %w{owner editor viewer}
   belongs_to :user
   belongs_to :password
+
+  validates :role, presence: true, inclusion: { in: ROLES }
 end
