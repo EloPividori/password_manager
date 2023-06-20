c961ee9d6615e5809dd1d9c9a07e592089e7549c
Eloise Pividori <eloisepividori@gmail.com>
2023-06-20 13:18:13 +0200

    finished password sharing and permissions

--- app/models/user_password.rb
+++ app/models/user_password.rb
@@ -3,9 +3,35 @@ class UserPassword < ApplicationRecord
   # Editor can update the password
   # Owner can update the password and share the password
 
-  ROLES = %w{owner editor viewer}
+  ROLES = %w{viewer editor owner}
   belongs_to :user
   belongs_to :password
 
   validates :role, presence: true, inclusion: { in: ROLES }
+
+  attribute :role, default: "viewer"
+
+  def owner?
+    role == "owner"
+  end
+
+  def viewer?
+    role == "viewer"
+  end
+
+  def editor?
+    role == "editor"
+  end
+
+  def editable?
+    owner? || editor?
+  end
+
+  def shareable?
+    owner?
+  end
+
+  def deletable?
+    owner?
+  end
 end
